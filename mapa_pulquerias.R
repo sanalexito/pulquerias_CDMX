396d2a6334927109b70b014c5c20f12f6b86cc2e
Alejandro Sánchez Peralta <65984679+sanalexito@users.noreply.github.com>
2024-04-25 12:28:13 -0600

    Update mapa_pulquerias.R

--- mapa_pulquerias.R
+++ mapa_pulquerias.R
@@ -1,22 +1,19 @@
 library(sf)
-my_sf <- read_sf("C:/Users/52552/Documents/Alexito/mapa_pulquerias/INEGI_DENUE_20042024.shp")
+library(tidyverse)
+library(leaflet)
+
+my_sf <- read_sf("~/INEGI_DENUE_20042024.shp")
 
 #me creo una variable extra como identificador
 my_sf$ID_MUN <- paste(my_sf$cve_ent, my_sf$cve_mun, sep = '.')
 
 
 par(mar = c(0, 0, 0, 0))
-plot(st_geometry(my_sf), col = "#f2f2f2", bg = "skyblue", lwd = 0.25, border = 0)
-
-library(ggplot2)
+#Miro los puntos en la retícula
 ggplot(my_sf) +
   geom_sf() 
 
-
-library(ggplot2)
-ggplot(my_sf) +
-  geom_sf(fill = "#69b3a2", color = "white") +
-  theme_void()
+#Se hace el vector de nombres para tener las zonas geográficas
 
 alcaldias <- c("Azcapotzalco",
                               "Coyoacán",
@@ -34,20 +31,20 @@ alcaldias <- c("Azcapotzalco",
                             "Cuauhtémoc",
                         "Miguel Hidalgo",
                    "Venustiano Carranza")
-pa_centroide <- st_read("C:/Users/52552/OneDrive - INEGI/Respaldo/varios/Medioambiente/banana/geojsons/Division Politica/CDMX_mpal.geojson") 
-ruta <- "C:/Users/52552/OneDrive - INEGI/Respaldo/varios/mariz/mapas/leaflet_v2024/"
+pa_centroide <- st_read("~/CDMX_mpal.geojson") 
+
 ctrd  <- st_centroid(pa_centroide) 
 
 ctrd <- ctrd[,c("NOM_MUN", "CVE_MUN", "CVE_ENT",   "geometry")]
+#Se crea el identificador para hacer el merge abajo
 ctrd$ID_MUN <- paste(ctrd$CVE_ENT, ctrd$CVE_MUN, sep = '.')
 
-
-
-ruta <- "C:/Users/52552/OneDrive - INEGI/Respaldo/varios/Medioambiente/banana/geojsons/Division Politica/CDMX_mpal.geojson"
+#Se carga el shape de la CDMX
+ruta <- "~/CDMX_mpal.geojson"
 mapa_a <- st_read(ruta) 
 
 aux <- as.data.frame(my_sf[,c("ID_MUN", "nom_estab")])
-
+#Aquí se pegan las bases usando el identificador que definimos
 a <- left_join(ctrd, aux, by = "ID_MUN") %>% filter(!is.na(nom_estab))
 a <- a %>%
   mutate(x = st_coordinates(a)[,1],
@@ -55,14 +52,16 @@ a <- a %>%
 
 class(a)
 
-
+#Primera vista de las alcaldías con locales
 a %>%
   ggplot() +
   geom_sf(data = pa_centroide) +
   geom_point(aes(x = x, y = y),
              color = "red")
 
-m1 <-leaflet(a) %>%
+#Distintas vistas del mapa interactivo:
+# 1. Este mapa muestra los locales agrupados por alcaldía
+leaflet(a) %>%
   addProviderTiles("CartoDB.DarkMatter") %>%
    addCircleMarkers(radius = ~2,
                    fillColor = "yellow",
@@ -73,8 +72,8 @@ m1 <-leaflet(a) %>%
                    popup = lapply(paste0("<b>Estado: </b>", a$NOM_MUN, "<br>" ),
                                   htmltools::HTML))
 
-
-m <- leaflet(my_sf) %>%
+#2. Este mapa pone todos los locales en sus respectivas localidades
+leaflet(my_sf) %>%
   addProviderTiles("CartoDB.DarkMatter") %>%
   addCircleMarkers(radius = 4,
                    fillColor = "yellow",
@@ -84,15 +83,8 @@ m <- leaflet(my_sf) %>%
                    color = "red",
                   
                    )
-data_map <- st_transform(mapa, crs = '+proj=longlat +datum=WGS84')
-
-
-m <- leaflet() %>%
-  addProviderTiles("CartoDB.DarkMatter") %>%
-  addPolygons(data_map)
-
-
-ruta <- "C:/Users/52552/OneDrive - INEGI/Respaldo/varios/Medioambiente/banana/geojsons/Division Politica/CDMX_mpal.geojson"
+#3. Mapa con el shape de la CDMX
+ruta <- "~/CDMX_mpal.geojson"
 mapa_a <- st_read(ruta) 
 
 # Transform to leaflet projection if needed
